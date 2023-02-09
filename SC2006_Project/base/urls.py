c8cce36288847cc49b3a8c8bdac2e5d2601e6b52
Runz99 <96276168+Runz99@users.noreply.github.com>
2023-02-10 01:22:49 +0800

    HTML templates for various pages
    Added various html template pages for the different functions and made some configurations

--- SC2006_Project/base/urls.py
+++ SC2006_Project/base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.login, name='login'),
+    path('find-nearest-restaurant/', views.findNearestRestaurant, name='FNR'),
+    path('leave-reviews/', views.leaveReviews, name='leaveReview'),
 ]
--- SC2006_Project/base/views.py
+++ SC2006_Project/base/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
-    return HttpResponse('Home Page')
+    context = {}
+    return render(request, 'base/home.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'base/login.html', context)
+
+def findNearestRestaurant(request):
+    context = {}
+    return render(request, 'base/find_nearest_restaurant.html', context)
+
+def leaveReviews(request):
+    context = {}
+    return render(request, 'base/leave_reviews.html', context)
+
