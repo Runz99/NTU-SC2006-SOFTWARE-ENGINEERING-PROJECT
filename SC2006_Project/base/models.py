012aaa71cb6310535f5e4b4d904fa3b799e6de17
THoldon <HTHAM001@e.ntu.edu.sg>
2023-03-21 15:13:36 +0800

    added review to models.py

--- SC2006_Project/base/models.py
+++ SC2006_Project/base/models.py
@@ -10,7 +10,15 @@ class restaurant(models.Model):
     lon = models.CharField(max_length = 100)
     opening_hours = models.CharField(max_length = 100)
     
-
     def __str__(self):
         return self.address
     
+class review(models.Model):
+    userName = models.CharField(max_length = 100)
+    restaurantName = models.CharField(max_length = 100)
+    address = models.CharField(max_length = 100)
+    restaurant_rating = models.CharField(max_length = 2)
+    restaurant_review = models.CharField(max_length = 100000)
+
+    def __str__(self):
+        return self.restaurant_review
