8270290e509548fe294ffd7c4769c64bbbae8b72
THoldon <HTHAM001@e.ntu.edu.sg>
2023-04-17 01:34:06 +0800

    remove token

--- SC2006_Project/SC2006_Project/settings.py
+++ SC2006_Project/SC2006_Project/settings.py
@@ -130,12 +130,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = ''
+EMAIL_HOST = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sdjango643@gmail.com'
-EMAIL_HOST_PASSWORD = 'bahwkvlbuffhmkac'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 #DEFAULT_FROM_EMAIL = 'password1@'
 
 GOOGLE_API_KEY = ''
